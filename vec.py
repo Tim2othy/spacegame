368c5119ebf06379abde38bd5af046268fa59c80
lumi-a <151774547+lumi-a@users.noreply.github.com>
2024-08-18 19:30:45 +0200

    Add comments to vec.py

--- vec.py
+++ vec.py
@@ -5,9 +5,11 @@ For the 2d-vector-type, and associated common operations.
 from typing import Literal
 import numpy as np
 
+# A two-dimensional vector with float64 entries.
 Vec = np.ndarray[tuple[int, Literal[2]], np.dtype[np.float64]]
 
 
+# TODO: I want this method used *everywhere*. Perhaps grep for "[" or something.
 def new_vec(x: float, y: float) -> Vec:
     return np.array([x, y])
 
