13ec28205b8591b2d1ed813c2d1c2e038ef93f18
lumi-a <151774547+lumi-a@users.noreply.github.com>
2024-08-18 19:57:59 +0200

    Partially vectorify bullet.py

--- bullet.py
+++ bullet.py
@@ -1,25 +1,26 @@
 import pygame
+from pygame.math import Vector2
 import math
 from enemy_info import BULLET_SPEED
 from ship import Ship
 
 class Bullet:
     def __init__(self, x: float, y: float, angle: float, ship: Ship):
-        self.pos = np.array([x, y])
+        self.pos = Vector2(x, y)
         bullet_speed = BULLET_SPEED + math.sqrt(ship.speed[0] ** 2 + ship.speed[1] ** 2)
-        self.speed = [
+        self.speed = Vector2(
             bullet_speed * math.cos(angle) + ship.speed[0] + ship.speed[0],
             bullet_speed * math.sin(angle) + ship.speed[1] + ship.speed[1],
-        ]
+        )
 
     def update(self):
-        self.pos[0] += self.speed[0]
-        self.pos[1] += self.speed[1]
+        # TODO: This must depend on ∆t
+        self.pos += self.speed
 
-    def draw(self, screen: pygame.Surface, camera_x: float, camera_y: float):
+    def draw(self, screen: pygame.Surface, camera_pos: Vector2):
         pygame.draw.circle(
             screen,
             (255, 255, 0),
-            (int(self.pos[0] - camera_x), int(self.pos[1] - camera_y)),
+            self.pos - camera_pos,
             3,
         )
