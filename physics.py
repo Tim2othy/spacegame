4a1f010bf68b1158c9ae9230b4dd65c23d2c3811
lumi-a <151774547+lumi-a@users.noreply.github.com>
2024-08-19 17:13:32 +0200

    Rename speed to vel

--- physics.py
+++ physics.py
@@ -2,7 +2,6 @@ from collections.abc import Sequence
 import pygame
 import pygame.camera
 from pygame.math import Vector2
-from pygame import draw
 from camera import Camera
 import math
 
@@ -11,16 +10,17 @@ GRAVITATIONAL_CONSTANT = 0.0006
 class PhysicalObject:
     """A physical object with dynamic position, dynamic speed, and constant nonzero mass."""
 
-    def __init__(self, pos: Vector2, speed: Vector2, mass: float):
+    def __init__(self, pos: Vector2, vel: Vector2, mass: float):
         self.pos = pos
         self.mass = mass
-        self.speed = speed
+        self.vel = vel
+        # TODO: Attribute `vel` was renamed from `speed`. GREP for any remnants of `speed`.
 
     def step(self, dt: float):
-        self.pos += dt * self.speed
+        self.pos += dt * self.vel
 
     def add_impulse(self, impulse: Vector2):
-        self.speed += impulse / self.mass
+        self.vel += impulse / self.mass
 
     def apply_force(self, force: Vector2, dt: float):
         self.add_impulse(force * dt)
@@ -56,13 +56,13 @@ class Disk(PhysicalObject):
     def __init__(
         self,
         pos: Vector2,
-        speed: Vector2,
+        vel: Vector2,
         density: float,
         radius: float,
         color: pygame.Color,
     ):
         mass = self.radius**3 * math.pi * 4 / 3
-        super().__init__(pos, speed, mass)
+        super().__init__(pos, vel, mass)
         self.radius = radius
         self.color = color
         self._radius_squared = radius**2
@@ -70,7 +70,7 @@ class Disk(PhysicalObject):
     def draw(self, camera: Camera):
         center = camera.world_to_camera(self.pos)
         radius = self.radius * camera.zoom
-        draw.circle(camera.surface, self.color, center, radius)
+        pygame.draw.circle(camera.surface, self.color, center, radius)
 
     def intersects_point(self, vec: Vector2) -> bool:
         return vec.magnitude_squared() < self._radius_squared
@@ -91,21 +91,21 @@ class Disk(PhysicalObject):
             delta = self.pos - disk.pos
             delta_magnitude = delta.magnitude()
             normal_vector = delta / delta_magnitude
-            self_speed_along_normal = self.speed.dot(normal_vector)
+            self_vel_along_normal = self.vel.dot(normal_vector)
 
             # Do not resolve if velocities are separating
-            if self_speed_along_normal > 0:
+            if self_vel_along_normal > 0:
                 return
 
             # Calculate restitution (bounciness)
             restitution = 1
 
             # Calculate impulse scalar
-            j = -(1 + restitution) * self_speed_along_normal
+            j = -(1 + restitution) * self_vel_along_normal
             j /= 1 / self.mass + 1 / disk.mass
 
             # Apply impulse
-            self.speed += normal_vector * j / self.mass
+            self.vel += normal_vector * j / self.mass
 
             # Move self outside other
             overlap = self.radius + disk.radius - delta_magnitude
@@ -131,11 +131,11 @@ class Asteroid(Disk):
     def __init__(
         self,
         pos: Vector2,
-        speed: Vector2,
+        vel: Vector2,
         density: float,
         radius: float,
     ):
-        super().__init__(pos, speed, density, radius, pygame.Color("gray"))
+        super().__init__(pos, vel, density, radius, pygame.Color("gray"))
 
     # TODO: Re-implement that the Asteroids stopped at the world-border.
     # Or should they wrap instead? Should *all* PhysicalObjects wrap?
