72a1748329cbe9bbb1b21b8b9f01463781055e78
lumi-a <151774547+lumi-a@users.noreply.github.com>
2024-08-19 15:08:18 +0200

    Create physics.py

---
+++ physics.py
@@ -0,0 +1,10 @@
+from pygame.math import Vector2
+
+
+class PhysicalObject:
+    """A physical object with dynamic position, dynamic speed, and constant nonzero mass."""
+
+    def __init__(self, pos: Vector2, speed: Vector2 = Vector2(0, 0), mass: float = 1):
+        self.pos = pos
+        self.mass = mass
+        self.speed = speed
