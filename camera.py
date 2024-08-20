6acfa6133ed1c9bfa4de86f5975ef872cf0a68d1
lumi-a <151774547+lumi-a@users.noreply.github.com>
2024-08-20 12:55:22 +0200

    Add `smoothly_transition_to` to Camera

--- camera.py
+++ camera.py
@@ -16,6 +16,24 @@ class Camera:
         self.zoom = zoom
         self.surface = surface
 
+    def smoothly_transition_to(
+        self, new_pos: Vector2, new_zoom: float, dt: float, transition_time: float
+    ):
+        """Smoothly transition the camera view to a new pos and zoom.
+
+        transition_speed (float): After this amount of time has passed, the camera will have fully
+        transitioned to the new location.
+        """
+
+        dist = self.pos.distance_to(new_pos)
+        self.pos.move_towards_ip(new_pos, dist * dt / transition_time)
+
+        # This makes it easier to write, please don't judge me
+        zoomy = Vector2(self.zoom, 0)
+        new_zoomy = Vector2(new_zoom, 0)
+        dist = abs(self.zoom - new_zoom)
+        self.zoom = zoomy.move_towards(new_zoomy, dist * dt / transition_time).x
+
     def world_to_camera(self, vec: Vector2) -> Vector2:
         """Transforms a worldspace-vector to its position on the camera's screen."""
         width, height = self.surface.get_size()
