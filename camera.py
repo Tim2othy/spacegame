7971efc3cfbccbf92d259c31bf0f14e930b72426
lumi-a <151774547+lumi-a@users.noreply.github.com>
2024-08-21 12:46:24 +0200

    Update all of Camera to use pygame.Rect

--- camera.py
+++ camera.py
@@ -1,7 +1,7 @@
 import pygame
 import pygame.gfxdraw
 from pygame.math import Vector2
-from pygame import Color
+from pygame import Color, Rect
 
 
 class Camera:
@@ -41,6 +41,21 @@ class Camera:
         dist = abs(self.zoom - new_zoom)
         self.zoom = zoomy.move_towards(new_zoomy, dist * dt / transition_time).x
 
+    def smoothly_focus_rect(self, rect: Rect, dt: float, transition_time: float = 0.25):
+        ratio = rect.width / rect.height
+        surface_width = self.surface.get_width()
+        surface_height = self.surface.get_height()
+        desired_ratio = surface_width / surface_height
+
+        if ratio > desired_ratio:
+            # Width dominates
+            zoom = surface_width / rect.width
+        else:
+            # Height dominates
+            zoom = surface_height / rect.height
+
+        self.smoothly_transition_to(Vector2(rect.center), zoom, dt, transition_time)
+
     def smoothly_focus_points(
         self,
         points: list[Vector2],
@@ -54,40 +69,12 @@ class Camera:
             points (list[Vector2]): The points to focus on
             buff (float): The padding around the points
         """
-        (enclosing_tl, enclosing_tr) = self._get_enclosing_rect(points)
+        enclosing_rect = self._get_enclosing_rect(points)
+        buffed_rect = enclosing_rect.inflate(2 * buff, 2 * buff)
+        self.smoothly_focus_rect(buffed_rect, dt, transition_time)
 
-        buff_offset = Vector2(buff, buff)
-        buffed_tl = enclosing_tl - buff_offset
-        buffed_br = enclosing_tr + buff_offset
-        buffed_size = buffed_br - buffed_tl
-        buffed_width = buffed_size.x
-        buffed_height = buffed_size.y
-        ratio = buffed_width / buffed_height
-
-        surface_width = self.surface.get_width()
-        surface_height = self.surface.get_height()
-        desired_ratio = surface_width / surface_height
-
-        if ratio > desired_ratio:
-            # Increase height
-            target_height = buffed_width / desired_ratio
-            addy = Vector2(0, (target_height - buffed_height) / 2)
-            buffed_tl -= addy
-            buffed_br += addy
-        else:
-            # Increase width
-            target_width = buffed_height * desired_ratio
-            addy = Vector2((target_width - buffed_width) / 2, 0)
-            buffed_tl -= addy
-            buffed_br += addy
-
-        new_center = (buffed_tl + buffed_br) / 2
-        new_zoom = surface_width / (buffed_br.x - buffed_tl.x)
-
-        self.smoothly_transition_to(new_center, new_zoom, dt, transition_time)
-
-    def _get_enclosing_rect(self, points: list[Vector2]) -> tuple[Vector2, Vector2]:
-        """Gets the smallest rectangle enclosing all points
+    def _get_enclosing_rect(self, points: list[Vector2]) -> Rect:
+        """Gets the smallest rectangle enclosing all points.
         Returns top-left and bottom-right coordinate of rect.
         """
         minx = miny = float("inf")
@@ -97,25 +84,12 @@ class Camera:
             maxx = max(maxx, point.x)
             miny = min(miny, point.y)
             maxy = max(maxy, point.y)
-        return (Vector2(minx, miny), Vector2(maxx, maxy))
+        return Rect((minx, miny), (maxx - minx, maxy - miny))
 
-    def _rectangle_intersects_screen(
-        self, top_left: Vector2, bottom_right: Vector2
-    ) -> bool:
-        """Determines whether the rectangle intersects screen.
-        Assumes that top_left.x <= bottom_right.x and top_left.y <= bottom_right.y.
-        """
-        (width, height) = self.surface.get_size()
-        own_top_left = Vector2(0, 0)
-        own_bottom_right = Vector2(width, height)
-        return all(
-            [
-                own_top_left.x <= bottom_right.x,
-                top_left.x <= own_bottom_right.x,
-                own_top_left.y <= bottom_right.y,
-                top_left.y <= own_bottom_right.y,
-            ]
-        )
+    def _rectangle_intersects_screen(self, rect: Rect) -> bool:
+        """Determines whether a screenspace-rectangle intersects the screen."""
+        own_rect = Rect((0, 0), self.surface.get_size())
+        return own_rect.colliderect(rect)
 
     def world_to_camera(self, vec: Vector2) -> Vector2:
         """Transforms a worldspace-vector to its position on the camera's screen."""
@@ -132,10 +106,8 @@ class Camera:
         x, y, r = int(ccenter.x), int(ccenter.y), int(cradius)
 
         # soft check for circle-screen-intersection:
-        offset = Vector2(r, r)
-        icenter = Vector2(x, y)
-        tl, br = icenter - offset, icenter + offset
-        if self._rectangle_intersects_screen(tl, br):
+        enclosing_rect = Rect((x - r, x - r), (2 * r, 2 * r))
+        if self._rectangle_intersects_screen(enclosing_rect):
             pygame.gfxdraw.aacircle(self.surface, x, y, r, color)
             pygame.gfxdraw.filled_circle(self.surface, x, y, r, color)
 
@@ -144,8 +116,8 @@ class Camera:
 
         cpoints = [self.world_to_camera(p) for p in points]
 
-        (enclosing_tl, enclosing_br) = self._get_enclosing_rect(cpoints)
-        if self._rectangle_intersects_screen(enclosing_tl, enclosing_br):
+        enclosing_rect = self._get_enclosing_rect(cpoints)
+        if self._rectangle_intersects_screen(enclosing_rect):
             pygame.gfxdraw.aapolygon(self.surface, cpoints, color)
             pygame.gfxdraw.filled_polygon(self.surface, cpoints, color)
 
@@ -168,19 +140,20 @@ class Camera:
         tstart, tend = self.world_to_camera(start), self.world_to_camera(end)
         startx, starty = int(tstart.x), int(tstart.y)
         endx, endy = int(tend.x), int(tend.y)
-        tl = Vector2(min(startx, endx), min(starty, endy))
-        br = Vector2(max(startx, endx), max(starty, endy))
+        enclosing_rect = Rect(
+            (min(startx, endx), min(starty, endy)),
+            (abs(startx - endx), abs(starty - endy)),
+        )
 
-        if self._rectangle_intersects_screen(tl, br):
+        if self._rectangle_intersects_screen(enclosing_rect):
             pygame.gfxdraw.line(self.surface, startx, starty, endx, endy, color)
 
     def draw_rect(self, color: Color, top_left: Vector2, bottom_right: Vector2):
         ttop_left = self.world_to_camera(top_left)
         tbottom_right = self.world_to_camera(bottom_right)
-        if self._rectangle_intersects_screen(ttop_left, tbottom_right):
-            pygame.gfxdraw.box(
-                self.surface, (ttop_left, tbottom_right - ttop_left), color
-            )
+        rect = Rect(ttop_left, tbottom_right - ttop_left)
+        if self._rectangle_intersects_screen(rect):
+            pygame.gfxdraw.box(self.surface, rect, color)
 
     def draw_text(
         self, text: str, pos: Vector2 | None, font: pygame.font.Font, color: Color
