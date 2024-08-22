983a65c7e67521db52bce09584fd505941891304
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2024-08-22 10:43:26 +0200

    added being pushed out of disks if ship overlaps back in, just to be safe

--- physics.py
+++ physics.py
@@ -172,4 +172,9 @@ class Disk(PhysicalObject):
 
         """This allows the ship to land on the planet. If impulse is small there is no damage"""
         damage = (max(0, impulse_scalar - 1300000)) * (1 - bounciness) * 1e-4
+
+        # Move self outside other
+        overlap = self.radius + disk.radius - delta_magnitude
+        self.pos += normal_vector * overlap
+
         return damage
