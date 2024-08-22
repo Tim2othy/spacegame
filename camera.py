fab78fca824ab7c064fcacbe476002c6f98536b3
lumi-a <151774547+lumi-a@users.noreply.github.com>
2024-08-22 12:20:34 +0200

    I guess font.render doesn't take kwargs

--- camera.py
+++ camera.py
@@ -251,7 +251,7 @@ class Camera:
             color (Color): Text's fill color
 
         """
-        rendered = font.render(text, antialias=True, color=color)
+        rendered = font.render(text, True, color)
         if pos is None:
             width, height = self.surface.get_size()
             pos = Vec2(
