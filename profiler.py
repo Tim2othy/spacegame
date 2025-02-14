efbce54371debe3e945fda5cf6dc2a1300559b8f
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-02-14 18:48:46 +0100

    Change profiler sorting-order to sort by time

--- profiler.py
+++ profiler.py
@@ -28,7 +28,7 @@ class Profiler:
         """Show the current state of the profiler."""
         total_time = sum(self.times[name] for name in self.times)
         outputs: list[str] = []
-        for name, t in sorted(self.times.items(), key=lambda entry: entry[0]):
             outputs.append(f"{name}: {t / total_time * 100:.2f}%")
+        for name, t in sorted(self.times.items(), key=lambda entry: entry[1]):
 
         return "\n".join(outputs)
