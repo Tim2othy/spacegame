f0e31c006110dff1ad3eb9f15ca6aea1bd150873
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-02-15 17:00:48 +0100

    Change assert-comments in test_camera.py

--- tests/test_camera.py
+++ tests/test_camera.py
@@ -10,8 +10,7 @@ EPSILON = 1e-8
 def test_smoothly_transition_to():
     camera = Camera(Vec2(1, 1), 1, pygame.Surface((2, 2)))
 
-    # Top-left corner of camera is at origin
-    assert camera.pos == Vec2(0, 0)
+    assert camera.pos == Vec2(0, 0), "Camera's top-left corner should have been at (0,0)"
 
     target_zoom = 2
     target_pos = Vec2(-3, -4)
@@ -25,15 +24,13 @@ def test_smoothly_transition_to():
         distance = camera.pos.distance_to(target_pos)
         zoom_difference = abs(camera.zoom - target_zoom)
 
-        # Test we're moving closer to our goal
-        assert distance < previous_distance
-        assert zoom_difference < previous_zoom_difference
+        assert distance < previous_distance, "Camera should move closer to target position"
+        assert zoom_difference < previous_zoom_difference, "Camera should move closer to target zoom"
         previous_distance = distance
         previous_zoom_difference = zoom_difference
 
-    # Test we're really close to our target
-    assert camera.pos.distance_to(target_pos) < EPSILON
-    assert abs(camera.zoom - target_zoom) < EPSILON
+    assert camera.pos.distance_to(target_pos) < EPSILON, "Camera should have reached target position"
+    assert abs(camera.zoom - target_zoom) < EPSILON, "Camera should have reached target zoom"
 
 
 def test_smoothly_focus_rectangle():
