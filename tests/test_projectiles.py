bb7272b85ff1dba0884e68f4106e2165711a8d66
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-02-15 17:18:34 +0100

    Better documentation of which projectile-type failed to hit

--- tests/test_projectiles.py
+++ tests/test_projectiles.py
@@ -1,13 +1,13 @@
 from pygame import Color
 from pygame.math import Vector2 as Vec2
 
-from projectiles import Missile, Rocket
+from projectiles import Bullet, Missile, Rocket
 from ship import Ship
 
 
 def test_homing():
     ship = Ship(
-        Vec2(1000, -500),
+        Vec2(1500, -500),
         vel=Vec2(0, 0),
         density=1,
         size=10,
@@ -17,14 +17,17 @@ def test_homing():
         bullet_speed=100,
     )
 
-    for projectile in [
-        Missile(Vec2(0, 0), Vec2(0, 0), Color(0, 0, 0), ship, "assets/missile.png"),
-        Rocket(Vec2(0, 0), Vec2(0, 0), Color(0, 0, 0), ship),
-    ]:
-        min_dist = 1e9
-        for _ in range(200):
-            projectile.step(0.1)
-            ship.step(0.1)
-            min_dist = min(min_dist, projectile.pos.distance_to(ship.pos))
+    def hits_ship(projectile: Bullet) -> bool:
+        """Simulate projectile for a while and return if it ever hits `ship`"""
+        for _ in range(1000):
+            projectile.step(0.01)
+            ship.step(0.01)
+            if ship.intersects_point(projectile.pos):
+                return True
+        return False
 
-        assert min_dist < ship.size, "Projectile should have hit the ship"
+    missile = Missile(Vec2(0, 0), Vec2(500, 0), Color(0, 0, 0), ship, "assets/missile.png")
+    rocket = Rocket(Vec2(0, 0), Vec2(500, 0), Color(0, 0, 0), ship)
+
+    assert hits_ship(missile), "Missile should have hit the ship"
+    assert hits_ship(rocket), "Rocket should have hit the ship"
