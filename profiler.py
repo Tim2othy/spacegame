335558c74537b87ea2b277d2310c29fc8bcd115a
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-02-15 16:46:05 +0100

    Change startup-behavior of profiler

--- profiler.py
+++ profiler.py
@@ -7,9 +7,9 @@ class Profiler:
     """A simple profiler."""
 
     def __init__(self) -> None:
-        """Start the profiler."""
-        self.most_recent_time = time.time()
-        self.most_recent_action = "setup"
+        """Create a new profiler."""
+        self.most_recent_time: None | float = None
+        self.most_recent_action: None | str = None
         self.times: dict[str, float] = {}
 
     def start(self, name: str) -> None:
@@ -18,8 +18,14 @@ class Profiler:
         The stopping-time is implicitly the time of the next start-call to `self`, with any name.
         """
         now = time.time()
+        if self.most_recent_action is None or self.most_recent_time is None:
+            self.most_recent_time = now
+            self.most_recent_action = name
+            return
+
         delta = now - self.most_recent_time
-        self.most_recent_action, action = name, self.most_recent_action
+        action = self.most_recent_action
+        self.most_recent_action = name
         if action in self.times:
             self.times[action] = self.times[action] + delta
         else:
@@ -28,9 +34,21 @@ class Profiler:
 
     def log(self) -> str:
         """Show the current state of the profiler."""
-        total_time = sum(self.times[name] for name in self.times)
+        times = self.times.copy()
+
+        # Insert currently running timer
+        if self.most_recent_action is not None and self.most_recent_time is not None:
+            now = time.time()
+            delta = now - self.most_recent_time
+            action = self.most_recent_action
+            if action in times:
+                times[action] = times[action] + delta
+            else:
+                times[action] = delta
+
+        total_time = sum(times[name] for name in times)
         outputs: list[str] = []
-        for name, t in sorted(self.times.items(), key=lambda entry: entry[1]):
+        for name, t in sorted(times.items(), key=lambda entry: entry[1]):
             outputs.append(f"{t / total_time * 100:2.0f}% {name}")
 
         return "\n".join(outputs)
