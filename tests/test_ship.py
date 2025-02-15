49dd5eb7bdc545d9586056381dfb812e2593901f
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-02-15 18:24:10 +0100

    Test ship-shooting for more ship-types

--- tests/test_ship.py
+++ tests/test_ship.py
@@ -2,7 +2,7 @@ from math import tau
 from pygame import Color
 from pygame.math import Vector2 as Vec2
 
-from ship import Ship
+from ship import Ship, BulletEnemy, RocketEnemy, MissileEnemy, ShipInput
 
 
 EPSILON = 1e-8
@@ -10,34 +10,37 @@ EPSILON = 1e-8
 
 def test_shooting():
     gun_cooldown = 0.123
-    bullet_speed = 0.456
     bullet_count = 10
-    ship = Ship(
-        Vec2(0, 0),
-        Vec2(0, 0),
-        1,
-        1,
-        Color(0, 0, 0),
-        Color(0, 0, 0),
-        gun_cooldown=gun_cooldown,
-        bullet_speed=bullet_speed,
-    )
-    ship.angle = tau / 8
-    ship.shooting = True
-    ship.shoot(bullet_count * gun_cooldown)
+    color = Color(0, 0, 0)
+    v0 = Vec2(0, 0)
+    # Enemies need to target *something*
+    target_ship = Ship(v0, v0, 1, 1, color, color, 1, 1)
 
-    assert len(ship.projectiles) == bullet_count, "Ship should have shot 10 bullets"
+    ships: list[Ship] = [
+        target_ship,
+        BulletEnemy(v0, v0, target_ship, v0, 1, 1, color, color),
+        RocketEnemy(v0, v0, target_ship, v0, color),
+        MissileEnemy(v0, v0, target_ship, v0, color),
+    ]
 
-    assert all(
-        abs((p.pos - ship.pos).angle_to(ship.get_faced_direction())) < EPSILON for p in ship.projectiles
-    ), "Bullets should be shot in the direction of the ship"
+    for ship in ships:
+        ship.gun_cooldown = gun_cooldown
+        ship.angle = tau / 8
+        ship.shooting = True
+        ship.shoot(bullet_count * gun_cooldown)
 
-    reference_delta = ship.projectiles[1].pos - ship.projectiles[0].pos
-    previous_projectile_pos = ship.projectiles[1].pos
-    for p in ship.projectiles[2:]:
-        delta = p.pos - previous_projectile_pos
-        assert (reference_delta - delta).length() < EPSILON, "Bullets should be evenly spaced"
-        previous_projectile_pos = p.pos
+        assert len(ship.projectiles) == bullet_count, "Ship should have shot 10 bullets"
+
+        assert all(
+            abs((p.pos - ship.pos).angle_to(ship.get_faced_direction())) < EPSILON for p in ship.projectiles
+        ), "Bullets should be shot in the direction of the ship"
+
+        reference_delta = ship.projectiles[1].pos - ship.projectiles[0].pos
+        previous_projectile_pos = ship.projectiles[1].pos
+        for p in ship.projectiles[2:]:
+            delta = p.pos - previous_projectile_pos
+            assert (reference_delta - delta).magnitude_squared() < EPSILON, "Bullets should be evenly spaced"
+            previous_projectile_pos = p.pos
 
 
 def test_movement():
