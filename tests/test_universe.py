a0a5bdefa800f66584ecec2bd71feb9752ed842a
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-02-15 19:45:47 +0100

    Ill-fated attempt at testing stable orbits

--- tests/test_universe.py
+++ tests/test_universe.py
@@ -1,5 +1,6 @@
-from universe import Universe, Asteroid
+from universe import Planet, Universe, Asteroid
 from pygame.math import Vector2 as Vec2
+from pygame import Color
 
 
 def test_mutual_bounce():
@@ -31,3 +32,47 @@ def test_mutual_bounce():
     assert 0 > asteroid_moving_rightward.vel.x > -start_vel * 0.99, (
         "The left asteroid should be moving to the left with less speed"
     )
+
+
+def test_stable_orbits():
+    for _ in range(10):
+        world = Vec2(500, 500)
+        planet = Planet(world / 2, 100, Color(0, 0, 0), 1)
+        universe = Universe(world, [planet], [], [], [])
+        universe.generate_asteroid(planet)
+        asteroid = universe.asteroids[0]
+
+        start = asteroid.pos
+        previous_distance = 0
+        was_previously_approaching = False
+        last_orbit_duration = None
+        last_orbit_timestamp = 0
+
+        epsilon = 1e-4
+
+        # 300 seconds
+        for t in range(300 * 100):
+            universe.step(0.01)
+            distance = start.distance_to(asteroid.pos)
+
+            if was_previously_approaching and distance > previous_distance:
+                # Our distance to `start` is locally minimal
+                assert distance < epsilon
+
+                period_length = t - last_orbit_timestamp
+                if last_orbit_duration is not None:
+                    assert abs(1 - last_orbit_duration / period_length) < epsilon, (
+                        "Orbits should take roughly the same duration"
+                    )
+
+                last_orbit_timestamp = t
+                last_orbit_duration = period_length
+
+                was_previously_approaching = False
+
+            was_previously_approaching = distance < previous_distance
+            previous_distance = distance
+
+        assert last_orbit_duration is not None, (
+            "The asteroid should complete at least one orbit in 300 seconds"
+        )
