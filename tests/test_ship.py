38f184369d9dc4f1da9e670dfd372c983a181b18
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-02-15 18:17:56 +0100

    Test ship.shoot()

---
+++ tests/test_ship.py
@@ -0,0 +1,42 @@
+from math import tau
+from pygame import Color
+from pygame.math import Vector2 as Vec2
+
+from ship import Ship
+
+
+EPSILON = 1e-8
+
+
+def test_shooting():
+    gun_cooldown = 0.123
+    bullet_speed = 0.456
+    bullet_count = 10
+    ship = Ship(
+        Vec2(0, 0),
+        Vec2(0, 0),
+        1,
+        1,
+        Color(0, 0, 0),
+        Color(0, 0, 0),
+        gun_cooldown=gun_cooldown,
+        bullet_speed=bullet_speed,
+    )
+    ship.angle = tau / 8
+    ship.shooting = True
+    ship.shoot(bullet_count * gun_cooldown)
+
+    assert len(ship.projectiles) == bullet_count, "Ship should have shot 10 bullets"
+
+    assert all(
+        abs((p.pos - ship.pos).angle_to(ship.get_faced_direction())) < EPSILON for p in ship.projectiles
+    ), "Bullets should be shot in the direction of the ship"
+
+    reference_delta = ship.projectiles[1].pos - ship.projectiles[0].pos
+    previous_projectile_pos = ship.projectiles[1].pos
+    for p in ship.projectiles[2:]:
+        delta = p.pos - previous_projectile_pos
+        assert (reference_delta - delta).length() < EPSILON, "Bullets should be evenly spaced"
+        previous_projectile_pos = p.pos
+
+
