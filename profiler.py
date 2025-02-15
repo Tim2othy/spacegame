a02adbb99c83f5fafe3716b13df9169a58d71156
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-02-16 00:03:31 +0100

    Add concerning TODO

--- profiler.py
+++ profiler.py
@@ -7,6 +7,12 @@ from collections.abc import Callable
 from dataclasses import dataclass, field
 
 
+# TODO: We should have a way of checking how much
+# of main's time we profile in total, lest we
+# end up optimising things that aren't great drains
+# to begin with.
+
+
 @dataclass
 class MethodProfile:
     """Stores profiling information for a method."""
