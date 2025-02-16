dc0a44569320bc5cd89ee3c045e19d2c58855cc6
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-02-16 01:14:25 +0100

    Update test_profiler

--- tests/test_profiler.py
+++ tests/test_profiler.py
@@ -1,24 +1,53 @@
 import re
 import time
 
-from profiler import Profiler
+from profiler import global_profiler
 
 
 def test_profiler() -> None:
-    profiler = Profiler()
+    @global_profiler.profile_method
+    def method_a() -> None:
+        time.sleep(0.02)
 
-    profiler.start("a")
-    time.sleep(0.06)
-    profiler.start("c")
-    profiler.start("a")
-    time.sleep(0.06)
-    profiler.start("b")
-    time.sleep(0.03)
+    @global_profiler.profile_method
+    def method_b() -> None:
+        time.sleep(0.01)
 
-    log = profiler.log()
-    assert re.match(
-        r""" 0% c
-(17|18|19|20|21|22|23)% b
-(77|78|79|80|81|82|83)% a""",
-        log,
-    ), "If this assertion fails, the above sleep-times might be too short, leading to precision-loss."
+    @global_profiler.profile_method
+    def method_c() -> None:
+        pass
+
+    method_a()
+    method_c()
+    method_a()
+    method_b()
+
+    epsilon = 0.05
+    stdev_cap = 1e-4
+    c_millisecond_cap = 15
+
+    stats = global_profiler.get_stats()
+    a_profile_count, b_profile_count, c_profile_count = 0, 0, 0
+    for m in stats:
+        if m.name == "test_profiler.<locals>.method_a":
+            a_profile_count += 1
+            assert abs(1 - m.total_time / 40) < epsilon, "Total time did not match expectation"
+            assert abs(1 - m.average_time / 20) < epsilon, "Average time did not match expectation"
+            assert abs(1 - m.median_time / 20) < epsilon, "Median time did not match expectation"
+            assert m.standard_deviation < stdev_cap, "Standard deviation should be low"
+        elif m.name == "test_profiler.<locals>.method_b":
+            b_profile_count += 1
+            assert abs(1 - m.total_time / 10) < epsilon, "Total time did not match expectation"
+            assert abs(1 - m.average_time / 10) < epsilon, "Average time did not match expectation"
+            assert abs(1 - m.median_time / 10) < epsilon, "Median time did not match expectation"
+            assert m.standard_deviation == 0, "Standard deviation should be exactly zero"
+        elif m.name == "test_profiler.<locals>.method_c":
+            c_profile_count += 1
+            assert 0 <= m.total_time < c_millisecond_cap, "Total time did not match expectation"
+            assert 0 <= m.average_time < c_millisecond_cap, "Average time did not match expectation"
+            assert 0 <= m.median_time < c_millisecond_cap, "Median time did not match expectation"
+            assert m.standard_deviation == 0, "Standard deviation should be exactly zero"
+
+    assert a_profile_count == 1, "method_a should have exactly one entry in stats"
+    assert b_profile_count == 1, "method_b should have exactly one entry in stats"
+    assert c_profile_count == 1, "method_c should have exactly one entry in stats"
