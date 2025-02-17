4230d8853872e651dd8cb34dfc595c9eafa0e99c
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-02-17 17:39:30 +0100

    Change test_mutual_bounce to be relative to both disks' velocity

--- tests/test_universe.py
+++ tests/test_universe.py
@@ -41,31 +41,32 @@ def test_planet_gravitation():
 
 
 def test_mutual_bounce():
-    start_y = 15
-    start_vel = 5
-    asteroid_moving_rightward = Asteroid(Vec2(10, start_y), Vec2(start_vel, 0), 1, 1)
-    asteroid_moving_leftward = Asteroid(Vec2(20, start_y), Vec2(-start_vel, 0), 2, 0.9)
+    # Bounces should be relative to the two asteroids'
+    # velocities:
+    for absolute_vel in [30 * Vec2(i, j) for i in range(-1, 2) for j in range(-1, 2)]:
+        start_y = 15
+        relative_vel = Vec2(5, 0)
+        # Boost both asteroids by absolute_vel
+        asteroid_moving_rightward = Asteroid(Vec2(10, start_y), absolute_vel + relative_vel, 1, 1)
+        asteroid_moving_leftward = Asteroid(Vec2(20, start_y), absolute_vel - relative_vel, 2, 0.9)
 
-    universe = Universe(Vec2(30, 30), [], [], [], [])
-    universe.asteroids.append(asteroid_moving_rightward)
-    universe.asteroids.append(asteroid_moving_leftward)
+        universe = Universe(Vec2(30, 30), [], [], [], [])
+        universe.asteroids.append(asteroid_moving_rightward)
+        universe.asteroids.append(asteroid_moving_leftward)
 
-    for _ in range(150):
-        universe.step(0.01)
+        for _ in range(150):
+            universe.step(0.01)
 
-    assert asteroid_moving_rightward.pos.x < asteroid_moving_leftward.pos.x, (
-        "The asteroids shouldn't fly past each other"
-    )
-    assert asteroid_moving_rightward.pos.y == asteroid_moving_leftward.pos.y == start_y, (
-        "The asteroid shouldn't move vertically at all"
-    )
-    assert asteroid_moving_rightward.vel.y == asteroid_moving_leftward.vel.y == 0, (
-        "The asteroid shouldn't move vertically at all"
-    )
-    # Test related to https://github.com/Tim2othy/spacegame/issues/9
-    assert 0 < asteroid_moving_rightward.vel.x < start_vel * 0.99, (
-        "The right asteroid should be moving to the right with less speed"
-    )
-    assert 0 > asteroid_moving_rightward.vel.x > -start_vel * 0.99, (
-        "The left asteroid should be moving to the left with less speed"
-    )
+        assert asteroid_moving_rightward.pos.x < asteroid_moving_leftward.pos.x, (
+            "The asteroids shouldn't fly past each other"
+        )
+        assert asteroid_moving_rightward.vel.y == asteroid_moving_leftward.vel.y == absolute_vel.y, (
+            "The asteroids shouldn't move vertically at all"
+        )
+        # Test related to https://github.com/Tim2othy/spacegame/issues/9
+        assert 0 < asteroid_moving_rightward.vel.x < (absolute_vel + relative_vel).x * 0.99, (
+            "The right asteroid should be moving to the right with less speed"
+        )
+        assert 0 > asteroid_moving_rightward.vel.x > (absolute_vel - relative_vel).x * 0.99, (
+            "The left asteroid should be moving to the left with less speed"
+        )
