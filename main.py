9c7e474d89e2ce9172a79c86a672b38683d206de
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-02-20 21:29:07 +0100

    Update callsites in main.py

--- main.py
+++ main.py
@@ -42,9 +42,7 @@ def universe_from_options(options: Options) -> tuple[Universe, list[PlayerShip]]
         PlayerShip(
             world_size_vec / 2,
             Vec2(0, 0),
-            1,
             10,
-            Color("darkslategray"),
             Color("orange"),
             ShipInput.arrows(),
             "assets/player_ship.png",
@@ -55,9 +53,7 @@ def universe_from_options(options: Options) -> tuple[Universe, list[PlayerShip]]
             PlayerShip(
                 world_size_vec / 2 + Vec2(50, 0),
                 Vec2(0, 0),
-                1,
                 10,
-                Color("blue"),
                 Color("yellow"),
                 ShipInput.wasd(),
                 "assets/player_ship.png",
@@ -104,7 +100,7 @@ def universe_from_options(options: Options) -> tuple[Universe, list[PlayerShip]]
     for _ in range(num_enemies):
         pos = Vec2(random.uniform(0, world_size_vec.x), random.uniform(0, world_size_vec.y))
         enemy_type = random.choices([BulletEnemy, RocketEnemy, MissileEnemy], [0.6, 0.2, 0.2])[0]
-        enemy_ships.append(enemy_type(pos, Vec2(0, 0), random.choice(player_ships), world_size_vec))
+        enemy_ships.append(enemy_type(pos, Vec2(0, 0), random.choice(player_ships)))
 
     universe = Universe(world_size_vec, planets, player_ships, enemy_ships, 100)
     for planet in planets:
