f934922fc446d7a86ea2dc235353d40b21886499
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-02-20 17:24:26 +0100

    Add newton's cradle test

--- tests/test_universe.py
+++ tests/test_universe.py
@@ -1,3 +1,4 @@
+import math
 from typing import TYPE_CHECKING
 
 from pygame import Color
@@ -70,3 +71,36 @@ def test_mutual_bounce():
         assert absolute_vel.x < asteroid_b.vel.x < (absolute_vel + relative_vel).x - 0.1, (
             "asteroid_b should be moving to the right with less speed"
         )
+
+
+def newtons_cradle():
+    # When we have a setup like this:
+    #  o->   oooo
+    # We expect it to look something like this afterwards:
+    #        oooo    o->
+    # (https://en.wikipedia.org/wiki/Newton's_cradle)
+    # At least, if bounciness==1, which is not the case here, but
+    # we still expect the rightmost asteroid to gain velocity afterwards.
+
+    world = Vec2(3000, 3000)
+    asteroid_radius = 50
+
+    # Do this 5 times for different directions
+    for i in range(5):
+        direction = Vec2()
+        direction.from_polar((asteroid_radius, i * math.tau / 5))
+
+        universe = Universe(world, [], [], [])
+        first_asteroid = Asteroid(world / 2, direction, 1, asteroid_radius)
+        universe.add_asteroids(first_asteroid)
+        other_asteroids = [
+            Asteroid(world / 2 + 2 * i * direction, Vec2(), 1, asteroid_radius) for i in range(2, 7)
+        ]
+        universe.add_asteroids(*other_asteroids)
+
+        # Run for 2 seconds
+        for _ in range(200):
+            universe.step(0.01)
+
+        assert first_asteroid.vel * direction < asteroid_radius, "First asteroid should have lost speed"
+        assert other_asteroids[-1].vel * direction > 0, "Last asteroid should have gained speed"
