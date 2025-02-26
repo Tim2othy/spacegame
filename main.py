31fc3122ff1f38067fffe752f508803c5e2f5d1d
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2025-02-27 00:57:35 +0100

    adjust enemy weights

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def universe_from_options(options: Options) -> tuple[Universe, list[PlayerShip]]
     enemy_ships: list[BulletEnemy] = []
     for _ in range(num_enemies):
         pos = Vec2(random.uniform(0, world_size_vec.x), random.uniform(0, world_size_vec.y))
-        enemy_type = random.choices([BulletEnemy, RocketEnemy, MissileEnemy], [0.6, 0.2, 0.2])[0]
+        enemy_type = random.choices([BulletEnemy, RocketEnemy, MissileEnemy], [0.5, 0.3, 0.2])[0]
         enemy_ships.append(enemy_type(pos, Vec2(0, 0), random.choice(player_ships)))
 
     universe = Universe(world_size_vec, planets, player_ships, enemy_ships, 100)
