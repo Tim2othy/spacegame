9135d796efdabd0741eb520091095a5d8b37c351
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-03-01 22:33:30 +0100

    Parametrize test_homing

--- tests/test_projectiles.py
+++ tests/test_projectiles.py
@@ -1,26 +1,19 @@
 from pygame import Color
 from pygame.math import Vector2 as Vec2
+import pytest
 
 from projectiles import Bullet, Missile, Rocket
 from ship import Ship
 
-# TODO: More class defaults.
 
-
-def test_homing():
+@pytest.mark.parametrize("bullet_type", [Missile, Rocket])
+def test_homing(bullet_type: type[Bullet]):
     ship = Ship(Vec2(512, -1024), Vec2(), 10)
+    projectile = bullet_type(Vec2(0, 0), Vec2(0, 0), Color(0, 0, 0), ship)
 
-    def hits_ship(projectile: Bullet) -> bool:
-        """Simulate projectile for a while and return if it ever hits `ship`"""
-        for _ in range(2000):
-            projectile.step(0.01)
-            ship.step(0.01)
-            if ship.intersects_point(projectile.pos):
-                return True
-        return False
-
-    missile = Missile(Vec2(0, 0), Vec2(500, 0), Color(0, 0, 0), ship)
-    rocket = Rocket(Vec2(0, 0), Vec2(0, 0), Color(0, 0, 0), ship)
-
-    assert hits_ship(missile), "Missile should have hit the ship"
-    assert hits_ship(rocket), "Rocket should have hit the ship"
+    for _ in range(2000):
+        projectile.step(0.01)
+        ship.step(0.01)
+        if ship.intersects_point(projectile.pos):
+            return
+    pytest.fail("The projectile should have hit the ship.")
