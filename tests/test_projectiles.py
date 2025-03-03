b3f32f904105a1937c6181013a417817410d8994
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2025-03-03 13:20:59 +0100

    add whitespace

--- tests/test_projectiles.py
+++ tests/test_projectiles.py
@@ -8,7 +8,7 @@ from ship import Ship
 
 @pytest.mark.parametrize("bullet_type", [Missile, Rocket])
 def test_homing(bullet_type: type[Bullet]):
-    ship = Ship(Vec2(512, -1024), Vec2(4,8), 10)
+    ship = Ship(Vec2(512, -1024), Vec2(4, 8), 10)
     projectile = bullet_type(Vec2(10, -15), Vec2(8, 29), Color(0, 0, 0), ship)
 
     for _ in range(2000):
