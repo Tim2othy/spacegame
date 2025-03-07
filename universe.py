5b649f18070caac8efceb112805ac2f87412f741
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-03-07 16:44:48 +0100

    Collapse apply_gravity_to_object loops into one
    (I noticed this due to a mypy-complaint of `body` (which has type `Star` due to the first loop) suddenly being assigned a value of type `Planet`)

--- universe.py
+++ universe.py
@@ -170,12 +170,11 @@ class Universe:
 
         """
         force_sum = Vec2(0, 0)
+
         # We can assume only one Star exists
-        for body in self.stars:
+        for body in chain(self.stars, self._nearby_planets(pobj)):
             force_sum += pobj.gravitational_force(body)
 
-        for body in self._nearby_planets(pobj):
-            force_sum += pobj.gravitational_force(body)
         pobj.apply_force(force_sum, dt)
 
     @global_profiler.profile_method
