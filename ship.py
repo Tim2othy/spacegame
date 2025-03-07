ad2894794bd578c70869cf73f0690ae5d08997c6
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-03-07 16:35:59 +0100

    Elevate scope of distance_to_target in ship.py
    They were only defined in the `if self.action_timer <= 0` branch, but distance_to_target_squared was also needed to determine wheter to set `self.shooting`.

--- ship.py
+++ ship.py
@@ -462,19 +462,16 @@ class BulletEnemy(Ship):
         """
         self.action_timer -= dt
         delta_target_ship = self.target_ship.pos_relative_to(self)
+        distance_to_target_squared = delta_target_ship.length_squared()
+        distance_to_target = math.sqrt(distance_to_target_squared)
 
         if self.action_timer <= 0:
-            if delta_target_ship == Vec2(0, 0):
-                delta_target_ship = Vec2(EPSILON, EPSILON)
-            distance_target_ship = delta_target_ship.length()
-            distance_squared = delta_target_ship.length_squared()
-
-            random_x = random.uniform(-distance_target_ship, distance_target_ship)
-            random_y = random.uniform(-distance_target_ship, distance_target_ship)
+            random_x = random.uniform(-distance_to_target, distance_to_target)
+            random_y = random.uniform(-distance_to_target, distance_to_target)
             # TODO@tim2othy: What is this supposed to do? Maybe add a comment?  ~lumi-a
             self.random_point = self._pos + (delta_target_ship + Vec2(random_x, random_y)) / 2
 
-            if distance_squared < ENEMY_VISUAL_RANGE_SQUARED:
+            if distance_to_target_squared < ENEMY_VISUAL_RANGE_SQUARED:
                 self.current_action = BulletEnemy.Action.accelerate_to_player
             else:
                 [self.current_action] = random.choices(
@@ -501,7 +498,7 @@ class BulletEnemy(Ship):
 
         self.shooting = (
             self.current_action == BulletEnemy.Action.accelerate_to_player
-            and distance_squared < ENEMY_FIRE_RANGE_SQUARED
+            and distance_to_target_squared < ENEMY_FIRE_RANGE_SQUARED
         )
         self.angle = math.degrees(math.atan2(force_direction.y, force_direction.x))
 
