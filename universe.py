a4ecccb5bccf565ab85e22c791e4d7fc9ead9b5e
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-03-08 14:34:54 +0100

    Update Star.__init__

--- universe.py
+++ universe.py
@@ -11,7 +11,7 @@ import pygame
 from pygame import Color
 from pygame.math import Vector2 as Vec2
 
-from physics import Disk, Particle, PhysicalObject
+from physics import Disk, MovingObject, Particle, PhysicalObject
 from profiler import global_profiler
 from ship import MissileEnemy, Ship
 
@@ -34,16 +34,10 @@ PLANET_ELLIPSIS_PARAMETER = 0.001
 class Star(Disk):
     """A stationary disk."""
 
-    def __init__(self, pos: Vec2, radius: float) -> None:
-        """Create a new star.
-
-        Args:
-            pos (Vec2): Fixed position
-            radius (float): Radius
-
-        """
-        color = Color(random.randint(200, 255), random.randint(150, 255), random.randint(0, 150))
-        super().__init__(pos, Vec2(0, 0), radius, color)
+    def __init__(self, relative_to: MovingObject, relative_pos: Vec2, relative_radius: float) -> None:
+        """Create a new star."""
+        star_color = Color(random.randint(200, 255), random.randint(150, 255), random.randint(0, 150))
+        super().__init__(relative_to, relative_pos, Vec2(0, 0), relative_radius, star_color)
 
 
 class Planet(Disk):
