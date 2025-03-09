d6754c4e619304320412998204f09b3c35f642cf
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-03-09 22:07:45 +0100

    Remove test_camera

--- tests/test_camera.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from pygame.math import Vector2 as Vec2
-
-from camera import Camera
-from physics import PosVel
-
-EPSILON = 1e-8
-
-ORIGIN = PosVel._new_origin_and_only_use_this_if_you_really_know_what_you_are_doing()
