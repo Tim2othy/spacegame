124783a3afe80f56a0aa5502743f7af610bf57bb
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-03-09 21:24:01 +0100

    Rebrand screenspace to surfacespace

--- camera.py
+++ camera.py
@@ -1,7 +1,7 @@
 """Class for rendering things to a surface, relative to a camera-position.
 
 worldspace == Coordinates in space
-screenspace == Coordinates on the screen
+surfacespace == Coordinates on the screen
 """
 
 from __future__ import annotations
@@ -33,13 +33,13 @@ class Camera(Pos):
         """Update the camera's position and zoom to track the object it's tracking."""
         self._shift(self._tracking.pos_relative_to(self))
 
-    def _rectangle_intersects_screen(self, rect: Rect) -> bool:
-        """Return whether a screenspace-rectangle intersects the camera's screen."""
+    def _rectangle_intersects_surface(self, rect: Rect) -> bool:
+        """Return whether a surfacespace-rectangle intersects the camera's surface."""
         # Inflate rect, to take care of edge-cases like zero width or height
         return self._surface_rect.colliderect(rect.inflate(1, 1))
 
-    def _world_to_screen(self, pos: Pos) -> Vec2:
-        """Transform a Pos to screenspace."""
+    def _world_to_surface(self, pos: Pos) -> Vec2:
+        """Transform a Pos to surfacespace."""
         return pos.pos_relative_to(self) * self.zoom
 
     @global_profiler.profile_method
@@ -48,13 +48,13 @@ class Camera(Pos):
         self._surface.fill(Color("black"))
 
     def draw_pixel(self, color: Color, pos: Pos) -> None:
-        """Draw a worldspace-pixel at position `pos` on screen."""
-        screenpoint = self._world_to_screen(pos)
-        if self._surface_rect.collidepoint(screenpoint):
-            self._surface.set_at((int(screenpoint.x), int(screenpoint.y)), color)
+        """Draw a worldspace-pixel at position `pos`."""
+        surfacepoint = self._world_to_surface(pos)
+        if self._surface_rect.collidepoint(surfacepoint):
+            self._surface.set_at((int(surfacepoint.x), int(surfacepoint.y)), color)
 
     def draw_circle(self, color: Color, center: Vec2, radius: float) -> None:
-        """Draw a worldspace-circle on screen.
+        """Draw a worldspace-circle.
 
         Args:
             color (Color): Border- and fill-color
@@ -62,20 +62,20 @@ class Camera(Pos):
             radius (float): Worldspace-radius of the circle
 
         """
-        ccenter, cradius = self._world_to_screen(center), radius * self.zoom
+        ccenter, cradius = self._world_to_surface(center), radius * self.zoom
         x, y, r = ccenter.x, ccenter.y, cradius
 
-        # soft check for circle-screen-intersection:
+        # soft check for circle-surface-intersection:
         enclosing_rect = Rect((x - r, y - r), (2 * r, 2 * r))
-        if self._rectangle_intersects_screen(enclosing_rect):
+        if self._rectangle_intersects_surface(enclosing_rect):
             pygame.draw.circle(self._surface, color, (x, y), r)
 
     def draw_polygon(self, color: Color, points: list[Pos]) -> None:
-        """Draw a polygon on screen."""
-        cpoints = [self._world_to_screen(p) for p in points]
-        # Soft check for points-screen-intersection:
+        """Draw a polygon."""
+        cpoints = [self._world_to_surface(p) for p in points]
+        # Soft check for point-surface-intersection:
         enclosing_rect = _get_enclosing_rect(cpoints)
-        if self._rectangle_intersects_screen(enclosing_rect):
+        if self._rectangle_intersects_surface(enclosing_rect):
             pygame.draw.polygon(self._surface, color, cpoints)
 
     def draw_line(self, color: Color, start: Vec2, end: Vec2, thickness: float) -> None:
@@ -98,7 +98,7 @@ class Camera(Pos):
             end - orthogonal,
             start - orthogonal,
         ]
-        # Need not check whether this is on-screen, as
+        # Need not check whether this is on-surface, as
         # draw_polygon does it for us
         self.draw_polygon(color, points)
 
@@ -111,9 +111,9 @@ class Camera(Pos):
             end (Vec2): Line's end-worldspace-point
 
         """
-        tstart, tend = self._world_to_screen(start), self._world_to_screen(end)
-        screen_rect = Rect((0, 0), self._surface.get_size())
-        clipped_line = screen_rect.clipline(tstart, tend)
+        tstart, tend = self._world_to_surface(start), self._world_to_surface(end)
+        surface_rect = Rect((0, 0), self._surface.get_size())
+        clipped_line = surface_rect.clipline(tstart, tend)
         if clipped_line:
             ((x1, y1), (x2, y2)) = clipped_line
             pygame.draw.line(self._surface, color, (x1, y1), (x2, y2))
@@ -128,9 +128,9 @@ class Camera(Pos):
             endy (float): Line's ending point
 
         """
-        tstart, tend = (self._world_to_screen(Vec2(x, starty)), self._world_to_screen(Vec2(x, endy)))
-        screen_rect = Rect((0, 0), self._surface.get_size())
-        clipped_line = screen_rect.clipline(tstart, tend)
+        tstart, tend = (self._world_to_surface(Vec2(x, starty)), self._world_to_surface(Vec2(x, endy)))
+        surface_rect = Rect((0, 0), self._surface.get_size())
+        clipped_line = surface_rect.clipline(tstart, tend)
         if clipped_line:
             ((x, y1), (_, y2)) = clipped_line
             pygame.draw.line(self._surface, color, (x, y1), (x, y2))
@@ -145,42 +145,20 @@ class Camera(Pos):
             y (float): Line's vertical position
 
         """
-        tstart, tend = (self._world_to_screen(Vec2(startx, y)), self._world_to_screen(Vec2(endx, y)))
-        screen_rect = Rect((0, 0), self._surface.get_size())
-        clipped_line = screen_rect.clipline(tstart, tend)
+        tstart, tend = (self._world_to_surface(Vec2(startx, y)), self._world_to_surface(Vec2(endx, y)))
+        surface_rect = Rect((0, 0), self._surface.get_size())
+        clipped_line = surface_rect.clipline(tstart, tend)
         if clipped_line:
             ((x1, y), (x2, _)) = clipped_line
             pygame.draw.line(self._surface, color, (x1, y), (x2, y))
 
     def draw_text(self, text: str, pos: Vec2 | None, font: pygame.font.Font, color: Color) -> None:
-        """Draw text on screen at screenspace-position, or centered on screen.
-
-        Args:
-            text (str): Text to render
-            pos (Vec2 | None): If Vec2, screenspace-position of text's top-left-corner,
-                if None, text will be centered on screen
-            font (pygame.font.Font): Font to be used
-            color (Color): Text's fill color
-
-        """
+        """Draw text at a surfacespace-position, or centered on the surface if not provided."""
         rendered = font.render(text, antialias=True, color=color)
         if pos is None:
             width, height = self._surface.get_size()
             pos = Vec2((width - rendered.get_width()) / 2, (height - rendered.get_height()) / 2)
         self._surface.blit(rendered, pos)
-
-    def draw_image(self, image: pygame.Surface, pos: Vec2) -> None:
-        """Draw an image on screen at screenspace-position.
-
-        Args:
-            image (pygame.Surface): Image to draw
-            pos (Vec2): Screenspace-position of the image's top-left-corner
-
-        """
-        zoomed_image = pygame.transform.scale(
-            image, (int(image.get_width() * self.zoom), int(image.get_height() * self.zoom))
-        )
-        self._surface.blit(zoomed_image, self._world_to_screen(pos))
 
 
 def _get_enclosing_rect(points: list[Vec2]) -> Rect:
