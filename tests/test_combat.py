82bef579b5349bf50e36cdaaabec1452ee9a052a
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2025-05-07 21:18:57 +0200

    add test_randomly_behaviour doesn't work yet though

--- tests/test_combat.py
+++ tests/test_combat.py
@@ -146,3 +146,23 @@ def test_markov_low_health_search() -> None:
             break
 
     assert player.health < HEALTH, "Enemy should have eventually found and damaged player"
+
+
+def test_randomly_behaviour() -> None:
+    """Test that an enemy moves towards a player when in random mode."""
+    universe = Universe(None, 100)
+    player = universe.add_player(PlayerConfig(relative_pos=Vec2(0, 0)))
+    enemy: BulletEnemy = universe.add_enemy(EnemyConfig(relative_pos=Vec2(1000, 0), target_ship=player), BulletEnemy)
+    distance_squared_0 = player.distance_squared_to(enemy)
+
+    for _ in range(6000):
+        enemy.ai.current_state = AIState.RANDOM
+        universe.step(0.01)
+
+    distance_squared_1 = player.distance_squared_to(enemy)
+    print(distance_squared_0**0.5)
+    print(distance_squared_1**0.5)
+    print(enemy.ai.seek_towards)
+    assert 4 == 5
+
+    assert distance_squared_0 > distance_squared_1, "Enemy should move towards player in random mode"
