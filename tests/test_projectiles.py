95be4d3d11c8048d065d4712b455d27324b30232
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2025-05-09 19:10:07 +0200

    fix test_homing

--- tests/test_projectiles.py
+++ tests/test_projectiles.py
@@ -1,3 +1,5 @@
+import random
+
 import pytest
 from pygame import Color
 from pygame.math import Vector2 as Vec2
@@ -12,11 +14,17 @@ ORIGIN = PosVel._new_origin_and_only_use_this_if_you_really_know_what_you_are_do
 @pytest.mark.parametrize("bullet_type", [Missile, Rocket])
 def test_homing(bullet_type: type[Missile | Rocket]) -> None:
     ship = Ship(ORIGIN, ShipConfig(relative_pos=Vec2(0, 0)))
-    projectile = bullet_type(ship, Vec2(-500, -500), Vec2(100, 0), Color(0, 0, 0), ship)
+    num_projectiles = 10
+    projectiles = []
+    for _ in range(num_projectiles):
+        start_pos = Vec2((random.random() - 0.5) * 200, (random.random() - 0.5) * 200)
+        start_vel = Vec2((random.random() - 0.5) * 200, (random.random() - 0.5) * 200)
+        projectiles.append(bullet_type(ship, start_pos, start_vel, Color(0, 0, 0), ship))
 
     for _ in range(2000):
-        projectile.step(0.01)
-        ship.step(0.01)
-        if ship.intersects_disk(projectile):
-            return
-    pytest.fail("The projectile should have hit the ship.")
+        for projectile in projectiles:
+            projectile.step(0.01)
+            ship.step(0.01)
+            if ship.intersects_disk(projectile):
+                return
+    pytest.fail("None of the projectiles hit the ship. At least one should have hit.")
